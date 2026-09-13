05bd1af2611764fbd83dc368e90a56f36ca32c81
Abiyoso Wibowo <abiyosowibowo@gmail.com>
2026-09-14 00:35:18 +0700

    Add nomor 2
    menambahkan template dari nomor 2 modul 03

--- modul03.php
+++ modul03.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="utf-8">
@@ -8,4 +8,26 @@
 <h1>Hello World</h1>
 <p>Belajar HTML</p>
 
+</html> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Hello World</title>
+<style type="text/css">
+h1 {
+color:orange;
+text-align:center;
+}
+p {
+font-family:"Times New Roman";
+font-size:40px;
+}
+</style>
+</head>
+<body>
+<h1>Hello World</h1>
+<p>Belajar HTML<p>
+</body>
 </html>
