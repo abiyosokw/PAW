6032e9894d4c3570968a3605a152d64cc53abecf
Abiyoso Wibowo <abiyosowibowo@gmail.com>
2026-09-14 13:55:39 +0700

    Menambahkan blockquote
    fungsinya seperi menabahkan petik dua seperti quote

--- modul03.php
+++ modul03.php
@@ -13,12 +13,30 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="utf-8">
-<title>Hello World</title>
-<link rel="stylesheet" href="style.css">
+    <meta charset="utf-8">
+    <title>Hello World</title>
+    
+    <style>
+    blockquote {
+        font-size: 24px;
+    }
+
+    blockquote::before {
+        content: open-quote;
+    }
+
+    blockquote::after {
+        content: close-quote;
+    }
+    </style>
 </head>
 <body>
 <h1>Hello World</h1>
 <p>Belajar HTML</p>
+
+    <blockquote>
+    I'd volunteer to go to prison, as long as there are books. Because with books I am free.
+    </blockquote>
+
 </body>
 </html>
