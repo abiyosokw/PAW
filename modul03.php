65b8e04dcf71e3e3f645d3b24219eecb4e5c03bd
Abiyoso Wibowo <abiyosowibowo@gmail.com>
2026-09-14 16:34:10 +0700

    Buat table

--- modul03.php
+++ modul03.php
@@ -50,7 +50,7 @@
 </body>
 </html> -->
 
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -67,6 +67,66 @@
         <li>U.A. High School </li>
         <li>Monsters University</li>
     </ol>
+
+</body>
+</html> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Latihan 6</title>
+  <style>
+    table, th, td {
+      border: 2px solid black;
+      border-collapse: collapse;
+      padding: 14px;
+    }
+  </style>
+</head>
+<body>
+
+  <table>
+    <tr>
+      <th rowspan="2">No.</th>
+      <th rowspan="2">Nama Sekolah</th>
+      <th colspan="2">Tahun</th>
+    </tr>
+    <tr>
+      <th>Masuk</th>
+      <th>Lulus</th>
+    </tr>
+    <tr>
+      <td>1.</td>
+      <td>TK Tadika Mesra</td>
+      <td>2010</td>
+      <td>2011</td>
+    </tr>
+    <tr>
+      <td>2.</td>
+      <td>Springfield Elementary School</td>
+      <td>2012</td>
+      <td>2018</td>
+    </tr>
+    <tr>
+      <td>3.</td>
+      <td>Kunugigaoka Junior High School</td>
+      <td>2019</td>
+      <td>2022</td>
+    </tr>
+    <tr>
+      <td>4.</td>
+      <td>U.A. High School</td>
+      <td>2023</td>
+      <td>2025</td>
+    </tr>
+    <tr>
+      <td>5.</td>
+      <td>Monsters University</td>
+      <td>2025</td>
+      <td>still studying</td>
+    </tr>
+  </table>
 
 </body>
 </html>
